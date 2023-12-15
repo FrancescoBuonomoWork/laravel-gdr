746931171d06920ea2915b5086e875cd59804001
Ste79ITA <nastri.s@gmail.com>
2023-12-15 16:27:21 +0100

    ADD seeder completed

--- database/seeders/Character.php
+++ database/seeders/Character.php
@@ -4,14 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Character;
+use Faker\Generator as Faker;
 
 class Character extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_character = new Character();
+
+            $new_character->name = $faker->name();
+            $new_character->bio = $faker->paragraph();
+            $new_character->defense = $faker->numberBetween(5, 99);
+            $new_character->speed = $faker->numberBetween(5, 99);
+            $new_character->hp = $faker->numberBetween(5, 99);
+
+            $new_character->save();
+        }
     }
 }
