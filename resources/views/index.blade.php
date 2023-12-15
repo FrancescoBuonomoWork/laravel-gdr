a25d7b42ef9ce06e2ca1e248cfd8f7c079be915c
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-12-15 17:33:24 +0100

    ADD - controller, index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,18 @@
 
 @section('content')
 
-    
+    <section class="container">
+        <div class="row">
+
+            @foreach ($characters as $character)
+                <div>
+                    <p>{{ $character->name}} </p>
+                    <p>{{ $character->bio}} </p>
+                    <p>Difesa: {{ $character->defense}} </p>
+                    <p>Velocità: {{ $character->speed}} </p>
+                    <p>Hp: {{ $character->hp}} </p>
+                </div>
+            @endforeach
+        </div>
+    </section>
 @endsection
