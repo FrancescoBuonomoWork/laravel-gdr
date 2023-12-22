f17b08ba6ccf8586b51de364797e8fe1b83f3061
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-12-22 16:19:30 +0100

    add --auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Guest\CharacterController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Guest\CharacterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,22 @@ use App\Http\Controllers\Guest\PageController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+// -------
 Route::get('/',[PageController::class,'index'])->name('homepage');
 
 
 Route::resource('characters', CharacterController::class);
+require __DIR__.'/auth.php';
